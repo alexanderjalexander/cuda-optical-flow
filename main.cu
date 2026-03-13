bcd15c40a9eb33b5394e4f7121e4191ed82922df
Alexander J <129627021+alexanderjalexander@users.noreply.github.com>
2026-03-13 13:37:21 -0400

    Final clang stuff that works on my end...

--- main.cu
+++ main.cu
@@ -1,12 +1,16 @@
 #include <opencv2/opencv.hpp>
 
+#include <filesystem>
 #include <iostream>
 #include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <filesystem>
+__global__ __forceinline__ void
+test_kernel()
+{
+}
 
 int
 main(int argc, char *argv[])
@@ -23,35 +27,41 @@ main(int argc, char *argv[])
      *
      * https://opencv.org/reading-and-writing-videos-using-opencv/
      */
-    
+
     std::filesystem::path current_dir = std::filesystem::current_path();
 
     std::filesystem::path example_file = "Example-Video.mp4";
     std::filesystem::path full_path = current_dir / "Example-Video.mp4";
-    
+
     cv::VideoCapture cap(full_path.string());
 
-    if (!cap.isOpened()) {
+    if (!cap.isOpened())
+    {
         std::cout << "Error: Could not open video file." << std::endl;
         return -1;
-    } else {
+    }
+    else
+    {
         std::cout << "Video file opened successfully!" << std::endl;
     }
- 
+
     // Read the first frame to confirm reading
     cv::Mat frame;
     bool ret = cap.read(frame);
- 
-    if (ret) {
- 
+
+    if (ret)
+    {
+
         // Display the frame using imshow
         cv::imshow("First Frame", frame);
-        cv::waitKey(0);  // Wait for a key press to close the window
-        cv::destroyAllWindows();  // Close the window
-    } else {
+        cv::waitKey(0);          // Wait for a key press to close the window
+        cv::destroyAllWindows(); // Close the window
+    }
+    else
+    {
         std::cout << "Error: Could not read the frame." << std::endl;
     }
- 
+
     // Release the video capture object
     cap.release();
 
