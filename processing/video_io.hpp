175cb0b9eb7074f36f63fc9cac74130bdd7e0735
Alexander J <129627021+alexanderjalexander@users.noreply.github.com>
2026-03-27 14:55:51 -0400

    refactoring video writing logic to another file

---
+++ processing/video_io.hpp
@@ -0,0 +1,22 @@
+#ifndef VIDEO_IO_H
+#define VIDEO_IO_H
+
+#include <opencv2/opencv.hpp>
+
+#include <filesystem>
+#include <vector>
+
+struct VideoInfo
+{
+    std::vector<cv::Mat> frames;
+    std::vector<cv::Mat> outputFrames;
+    double fps;
+    int totalFrames;
+    int width;
+    int height;
+};
+
+int readVideo(VideoInfo &video, std::filesystem::path video_path);
+int writeVideo(VideoInfo &video, std::filesystem::path video_path);
+
+#endif
